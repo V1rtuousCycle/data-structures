dc9dddd96121a59f27c9b4b175cd01565b67f303
Anton V Shtylman <AntonVS.Professional@gmail.com>
2017-10-12 11:36:13 -0700

    

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,46 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+// {}
+// push(a) // {0:a}
+// push(b) //  {0:a, 1 : b} size =2
+// pop     // {0:a, 1:b} size = 1 // return 1:b
+// pop //  {0:a, 1:b, 2:c, 3:d, 4:e} size = 0 // 0 : a
+// push( c) //
+//
+
+
+
+var stackMethods = {
+  size: function() {
+    return this.stackSize >= 0 ? this.stackSize : 0;
+  },
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+  pop: function() {
+    this.stackSize--;
+    this.offSet++;
+    return this.storage[this.stackSize];
+  }
 };
 
-var stackMethods = {};
+var Stack = function() {
+  var newStack = Object.create(stackMethods);
+  newStack.stackSize = 0;
+  newStack.offSet=0;
+  newStack.storage = {};
+
+
+  // var testClosure='found';
+  // newStack.findClosure = function () {
+  //   console.log('closure lookup = ',testClosure);
+  // }
+
+  return newStack;
+};
+
+
+
+var stackInstance=Stack();
+console.log(stackInstance.size)
 
 

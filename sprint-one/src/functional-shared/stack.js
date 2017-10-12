5362270e9d9907313ad2601b4ca3618a88ef0d42
Anton V Shtylman <AntonVS.Professional@gmail.com>
2017-10-11 21:14:50 -0700

    Completed functional and functional-shared prompts for stack data structure.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,53 @@
 var Stack = function() {
+  var newInstance = {};
+  newInstance.storage = {};
+  newInstance.stackSize = 0;
+
+  // newInstance.size = sizeFunc;
+  // newInstance.push = push;
+  // newInstance.pop = pop;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  extend(newInstance,sharedMethods)
+  console.log('newInstance = ',newInstance)
+  return newInstance;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+}
 
+var sharedMethods = {
+  push : function(val) {
+    this.storage[this.stackSize] = val;
+    this.stackSize++;
+  },
+  pop : function() {
+    this.stackSize--;
+    return this.storage[this.stackSize];
+  },
+  size : function() {
+    return this.stackSize >= 0 ? this.stackSize : 0;
+  }
+}
 
+  // var push = function(val) {
+  //   this.storage[size] = val;
+  //   this.size++;
+  // };
+  // var pop = function() {
+  //   this.size--;
+  //   return this.storage[size];
+  // };
+  // var sizeFunc = function() {
+  //   return this.stackSize ;
+  //   //return (this.size >= 0 ? this.size : 5);
+  // }
+
+// var storage = {};
+// storage[stackSize] = 22;
+// var stackSize = 27;
+// var oneStack = Stack();
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,38 @@
 var Stack = function() {
-  var someInstance = {};
-
-  // Use an object with numeric keys to store values
+  var newInstance = {};
   var storage = {};
+  var size = 0;
 
-  // Implement the methods below
-  someInstance.push = function(value) {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  // newInstance.size = sizeFunc;
+  // newInstance.push = push;
+  // newInstance.pop = pop;
+
+  newInstance.push = function(val) {
+    storage[size] = val;
+    size++;
   };
-
-  someInstance.pop = function() {
+  newInstance.pop = function() {
+    size--;
+    return storage[size];
   };
+  newInstance.size = function() {
+    return (size >= 0 ? size : 0);
+  }
 
-  someInstance.size = function() {
-  };
+  return newInstance;
+  // var someInstance = {};
 
-  return someInstance;
 };
+
+// pop() - error/undefined - undefined
+// push(22) - return - {0 : 22}
+// size - 1 - {0 : 22}
+// push(17) - return - {0:22, 1:17}
+// pop - 17 - {0:22}
+// pop - 22 - {}
+// size - 0 - {}
+
+
+

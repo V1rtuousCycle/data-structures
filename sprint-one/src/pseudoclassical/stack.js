663c26b9b0fd576c59a1dfab79241f7165ffcb54
Anton V Shtylman <AntonVS.Professional@gmail.com>
2017-10-12 19:10:03 -0700

    Completed the entire Stack implementation. Moving on to Queue

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.stackSize= 0;
+  this.storage= {};
+
 };
 
+
+Stack.prototype = {
+  push : function (value) {
+    this.storage[this.stackSize]=value;
+    this.stackSize++;
+
+  },
+  pop : function () {
+    this.stackSize--;
+    return this.storage[this.stackSize];
+  },
+  size : function () {
+    return this.stackSize >= 0 ? this.stackSize : 0 ;
+  },
+  constructor : Stack
+};
+
+//Stack.prototype.constructor = Stack;
+
+console.log(Stack.prototype.constructor);
+//var newStack = new Stack(0,{});
 

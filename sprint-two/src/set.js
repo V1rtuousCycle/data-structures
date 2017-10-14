101c113ffd6fb3142316f0e46ad1f3f7d36261b0
Anton V Shtylman <AntonVS.Professional@gmail.com>
2017-10-14 16:40:31 -0700

    Completed Set data structure.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
-
+// only takes string values
 setPrototype.add = function(item) {
+  this._storage[item] = 'santa clause';
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
-
+// only takes string values
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ All the methods are constant time.
  */
+
+/*
+ _storage = {}
+
+ _storage.value = '';
+
+ => retireval
+this._storage.hasOwnProperty(value)
+
+ */
+
